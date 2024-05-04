c022ae7b357c5ab716b144dff36fdb31321d9e6f
Alid3456778 <AlidShaikh05@gmail.com>
2024-05-04 08:13:37 +0530

    Adding New Project file

---
+++ Project/gym_project.cpp
@@ -0,0 +1,29 @@
+#include<iostream.h>
+#include<conio.h>
+#include<string.h>
+
+class Basic{
+int a;
+public:
+	Basic(){
+	cout<<"Enter the data "<<endl;
+	cin>>a;
+	}
+	void put_data(){
+	cout<<"The Enter Data is "<<a<<endl;
+	}
+
+};
+class Derive{
+public:
+
+};
+
+
+void main(){
+clrscr();
+
+
+
+getch();
+}
