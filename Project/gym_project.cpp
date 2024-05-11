551171f7ff184594933db6d5c929eea10ad8007b
Alid3456778 <AlidShaikh05@gmail.com>
2024-05-11 10:49:40 +0530

    updating and adding admin class

--- Project/gym_project.cpp
+++ Project/gym_project.cpp
@@ -1,56 +1,76 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-
 using namespace std;
 
-class person {
+class person
+{static int latest_id;
 public:
-    int id;
+     int id;
     long long int phone_no;
     string name, address;
 
-    void get_person_data() {
-        cout << "Getting Data " << endl << endl;
+    static int get_next_id() {
+        return ++latest_id;
+    }
+       
+    void get_person_data()
+    {
+        id = get_next_id();
+        cout << "Getting Data " << endl
+             << endl;
         cout << "Enter the Name " << endl;
         cin >> name;
         cout << "Enter the Phone Number " << endl;
         cin >> phone_no;
         cout << "Enter the Address " << endl;
-        cin.ignore(); // Ignore any previous newline character
+        cin.ignore(); 
         getline(cin, address);
-        id++;
+        
     }
 
-    void put_person_data(ofstream& file) {
-        cout << endl << endl << "Putting Data " << endl << endl;
+    void put_person_data(ofstream &file)
+    {
+        cout << endl
+             << endl
+             << "Putting Data " << endl
+             << endl;
         cout << "Id no : " << id << endl;
         cout << "Name  : " << name << endl;
         cout << "Phone No : " << phone_no << endl;
         cout << "Address : " << address << endl;
 
-        // Write data to the file
+        
         file << "ID: " << id << ", Name: " << name << ", Phone No: " << phone_no << ", Address: " << address << endl;
     }
 };
-
-class packet {
+int person::latest_id=0;
+class packet
+{
 public:
     long int price;
     int duration, choice;
 
-    void packet_accept() {
+    void packet_accept()
+    {
         cout << "What packet Do you Want " << endl;
-        cout << "1. Duration :1 Year " << endl << " Price :2700" << endl;
-        cout << "2. Duration :6 Month " << endl << " Price : 2000" << endl;
-        cout << "3. Duration :3 Month " << endl << " Price : 1500" << endl;
-        cout << "4. Duration :2 Month " << endl << " Price : 700" << endl;
-        cout << "5. Duration :1 Month " << endl << " Price : 500" << endl;
+        cout << "1. Duration :1 Year " << endl
+             << " Price :2700" << endl;
+        cout << "2. Duration :6 Month " << endl
+             << " Price : 2000" << endl;
+        cout << "3. Duration :3 Month " << endl
+             << " Price : 1500" << endl;
+        cout << "4. Duration :2 Month " << endl
+             << " Price : 700" << endl;
+        cout << "5. Duration :1 Month " << endl
+             << " Price : 500" << endl;
 
-        cout << endl << "Enter the Choice " << endl;
+        cout << endl
+             << "Enter the Choice " << endl;
         cin >> choice;
 
-        switch (choice) {
+        switch (choice)
+        {
         case 1:
             price = 2700;
             break;
@@ -72,34 +92,195 @@ public:
     }
 };
 
-class customer : public person, public packet {
+class customer : public person, public packet
+{
 public:
-    void get_customer_data() {
+    void get_customer_data()
+    {
+        ofstream outFile("customer_data.txt", ios::app); 
+        if (!outFile)
+        {
+            cerr << "Error opening file." << endl;
+            exit;
+        }
         get_person_data();
         packet_accept();
+        outFile.close();
     }
 
-    void put_customer_data(ofstream& file) {
-        put_person_data(file);
+    void put_customer_data()
+    {
+                ofstream outFile("customer_data.txt", ios::app); 
+        if (!outFile)
+        {
+            cerr << "Error opening file." << endl;
+            exit;
+        }
+        put_person_data(outFile);
         cout << "Amount is " << price;
 
-        // Write price to the file
-        file << ", Amount: " << price << endl;
+       
+        outFile << ", Amount: " << price << endl;
+    }
+
+    void show_customer_data()
+    {
+        int search_id;
+        cout << "Enter ID to search: ";
+        cin >> search_id;
+
+        ifstream inFile("customer_data.txt");
+        if (!inFile)
+        {
+            cerr << "Error opening file." << endl;
+            return;
+        }
+
+        string line;
+        while (getline(inFile, line))
+        {
+            size_t found = line.find("ID: " + to_string(search_id));
+            if (found != string::npos)
+            {
+                cout << "Customer Data:" << endl;
+                cout << line << endl;
+                return;
+            }
+        }
+
+        cout << "Customer with ID " << search_id << " not found." << endl;
+    }
+
+    void show_all_customer_data()
+    {
+        ifstream inFile("customer_data.txt");
+        if (!inFile)
+        {
+            cerr << "Error opening file." << endl;
+            return;
+        }
+
+        string line;
+        cout << "All Customer Data:" << endl;
+        while (getline(inFile, line))
+        {
+            cout << line << endl;
+        }
+
+        inFile.close();
     }
 };
 
-int main() {
-    ofstream outFile("customer_data.txt", ios::app); // Open file for appending
-    if (!outFile) {
-        cerr << "Error opening file." << endl;
-        return 1;
+class Employee : public customer
+{
+public:
+    void get_employee_data()
+    {
+        get_person_data();
+        save_employee_data();
     }
 
-    customer c1;
-    c1.get_customer_data();
-    c1.put_customer_data(outFile);
+    void save_employee_data()
+    {
+        ofstream outFile("employee_data.txt", ios::app); 
+        if (!outFile)
+        {
+            cerr << "Error opening file." << endl;
+            return;
+        }
 
-    outFile.close();
+        outFile.close();
+        cout << "Employee data saved successfully." << endl;
+    }
+
+    void put_employee_data(){
+                ofstream outFile("employee_data.txt", ios::app); 
+        if (!outFile)
+        {
+            cerr << "Error opening file." << endl;
+            return;
+        }
+        put_person_data(outFile);
+        outFile.close();
+    }
+};
+
+class Admin:public Employee{
+    public:
+    string name,password;
+
+    void admin_entery(){
+        cout<<"Enter the Name "<<endl;
+        cin>>name;
+        cout<<"Enter the Password "<<endl;
+        cin>>password;
+
+         ofstream outFile("Admin_data.txt", ios::app); 
+        if (!outFile)
+        {
+            cerr << "Error opening file." << endl;
+            return;
+        }
+        outFile<<"Name :"<<name<<" Password :"<<password;
+        outFile.close();
+        cout << "Admin data saved successfully." << endl;
+        
+    }
+
+    void admin_only(){
+        string input_name, input_password;
+        cout << "Enter your name: ";
+        cin >> input_name;
+        cout << "Enter your password: ";
+        cin >> input_password;
+
+        ifstream inFile("Admin_data.txt");
+        if (!inFile) {
+            cerr << "Error opening file." << endl;
+            exit;
+        }
+
+        string line;
+        while (getline(inFile, line)) {
+            size_t name_pos = line.find("Name: ");
+            if (name_pos != string::npos) {
+                size_t password_pos = line.find(" Password: ");
+                string stored_name = line.substr(name_pos + 6, password_pos - (name_pos + 6));
+                string stored_password = line.substr(password_pos + 11);
+                if (input_name == stored_name && input_password == stored_password) {
+                    cout << "Login successful." << endl;
+                    inFile.close();
+
+
+                    exit;
+                }
+            }
+        }
+
+        cout << "Invalid name or password." << endl;
+        inFile.close();
+        exit;
+    }
+    
+};
+
+int main()
+{
+
+    // customer c1;
+    // c1.get_customer_data();
+    // c1.put_customer_data();
+
+    
+
+    // c1.show_customer_data();
+
+    // Employee e1;
+    // e1.get_employee_data();
+    // e1.put_employee_data();
+
+    Admin a1;
+    a1.admin_only();
 
     return 0;
 }
